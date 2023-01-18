e5f89f4aefcf88844a25de3e4840a3c7488216fd
Charles Young <cjyoung2001@yahoo.com>
2023-01-17 20:10:02 -0800

    added filter mask (UNTESTED)

--- ai.py
+++ ai.py
@@ -1,24 +1,19 @@
 import numpy as np
 import sapai as sp
-import math
 
-PETS_AVAIL = 10
-ITEMS_AVAIL = 2
 TEAM_SLOTS = 5
+PETS_AVAIL = 10
 STATUSES = 1
-
-def sigmoid(x):
-    return 1 / (1 + pow(math.e, -x))
-
+ITEMS_AVAIL = 2
+ROUNDS = 2
 
 def relu(x):
-    return np.maximum(np.zeros([1, x.shape[1]]), x)
+    return max(0, x)
 
 
 def generate_wt(x, y):
-    l = np.random.rand(x * y)
-    l = l.reshape(x, y)
-    # print(l.shape)
+    l = np.random.rand(x*y)
+    l.reshape([x, y])
     return l
 
 
@@ -37,23 +32,85 @@ class AI:
 
     def forward(self, input_data):
         m1 = np.dot(input_data, self.w1)
-        m1 = sigmoid(m1)
         m2 = np.dot(m1, self.w2)
-        m2 = sigmoid(m2)
         o = np.dot(m2, self.w3)
-        # to check what pets exist/don't exist, look at input_data[0, 11 + 13n] for 0 <= n <= 4
-        empty_slot = False
-        for i in range(5):
-            if not input_data[0, 11 + 13 * i]:  # if slot on team is empty
-                empty_slot = True
-                o[0, 41 + i] = 0  # can't sell pet that isn't there
-                for j in range(2): # can't buy food for pet that isn't there
-                    o[0, PETS_AVAIL*2 + (TEAM_SLOTS + 1) * i] = 0
-
-        for i in range(10):
-            if not empty_slot and not input_data[0, 65 + 2 * i]:  # if no slots or upgrades exist
-                o[0, 2 * i] = 0  # can't buy this pet
-
         return o
+
+"""
+State data format:
+team_slots(pets_avail+2+statuses)-1
+    Team pet information
+2(pets_avail+items_avail) 65
+    Shop information
+Round # 89
+Gold # 90
+
+mask data format:
+pets_avail(buy+freeze+unfreeze)
+    actions on shop pets
+items_avail(team_slots+freeze+unfreeze) 30
+    actions on shop items
+moves 44
+    ways team pets can be moved
+combine 64
+    ways where two team pets can be combined
+team_slots(sell, upgrade) 84
+    actions on team pets
+"""
+def filter_mask(state):
+    mask = np.zeros([89, 1])
+    # Can only buy pets if empty space exists and gold >= 3
+    empty_slots = []
+    for i in range(TEAM_SLOTS):
+        if state[0, 11+13*i] == 0: #  checks if spot is empty
+            empty_slots.append(i)
+        else:
+            # enables sell action for pet
+            mask[84 + 2*i] = 1
+
+            for slot in range(TEAM_SLOTS):
+                for pet in range(PETS_AVAIL):
+                    # checks if same pet is in shop
+                    if state[0, pet + 13 * slot] and state[0, 65 + 2*pet]:
+                        # enables upgrade action for pet
+                        mask[85 + 2*slot] = 1
+
+                    # checks which items are available to buy for the pet
+            for item in range(ITEMS_AVAIL):
+                if state[85 + 2*item]:
+                    if state[86 + 2*item]: #  checks if the items are frozen or not
+                        mask[35 + 6*item] = 1
+                    else:
+                        mask[36 + 6*item] = 1
+
+                    mask[30 + i + 6*item] = 1
+
+    # checks if pets can be bought
+    if state[0, 90] >= 3 and len(empty_slots) > 0:
+        for i in range(PETS_AVAIL):
+            mask[3*i, 0] = 1
+
+    for i in range(PETS_AVAIL):  # checks which pets are in the shop
+        if state[65 + 2*i]:
+            if state[66 + 2 * i] == 0:  # checks if they are frozen
+                mask[3*i+1, 0] = 1
+            else:  # assumes unfrozen otherwise
+                mask[3*i+2, 0] = 1
+        else:
+            mask[3*i, 0] = 0
+
+    move = 0
+    for slot in range(TEAM_SLOTS):
+        for slot2 in range(slot+1, TEAM_SLOTS):
+            for pet in range(PETS_AVAIL):
+                # checks if two slots are different
+                if state[0, pet + 13 * slot] != state[0, pet + 13 * slot2]:
+                    mask[44 + move] = 1
+                    break
+                # checks if slots are both nonempty
+                elif state[0, pet + 13 * slot]:
+                    mask[64 + move] = 1
+
+    return mask
 
 
