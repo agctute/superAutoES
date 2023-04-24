e3dfb164f79dde47bf56fe7a9ace48ba92ea3bc6
Charles Young <cjyoung2001@yahoo.com>
2023-04-24 02:04:36 +0000

    STYLE: added significant comments to convert.py

--- convert.py
+++ convert.py
@@ -19,12 +19,31 @@ Round # 101
 Gold # 102
 """
 p = Player()
-# print(team[0])
+# TEAM_SLOTS: total number of slots in the team (usually 5)
+# PETS_AVAIL: number of pets available in the current round
+# STATUSES: number of statuses that a pet can potentially have in the current round
+# ITEMS_AVAIL: number of items available in the current round
+# ROUND: current round
+# GOLD: current gold
 
-# print(p.state['team']['team'][0]['pet'])
-# print(p.state['shop']['slots'][0])
-
-
+# This function converts the player's state into a numpy array, which only considers the first 3 rounds of the game
+# The array is of shape (1, 103)
+# The array is of the following format, where capital connected words denote
+# variables or features from the Player class:
+# the first TEAM_SLOTS(PETS_AVAIL + 2 + STATUSES) - 1 entries the state of pets in the team
+# the ith (PET_AVAIL + 2 + STATUSES) entries represent the state of the pet in the ith slot, 
+# with 5 total potential pets. if the slot is empty, all entries are 0
+# This pet state is represented by the name of the pet, its health and attack, and what status it has
+# the name of the pet is one-hot encoded, so are the statuses.
+# 
+# The next 3(PETS_AVAIL + ITEMS_AVAIL) entries represent the state of pets in the shop.
+# Since we are only considering the first three rounds, thats was the 3 is for.
+# the ith (PET_AVAIL + 2 + STATUSES) entries represent the state of the pet in the ith slot, 
+# for every pet and item that can be in the shop, the "3" declares:
+# the number of pets/foods of that name in the shop
+# whether one of the pets is frozen
+# the cost of the pet
+# The last two entries are the round number and the gold number
 def conv_to_arr(player):
     res = np.zeros(103)
     res[95:100:3] = 3
@@ -47,6 +66,7 @@ def conv_to_arr(player):
                 res[curr] = 1
             curr += 1
 
+    # Shop information  
     for shop_slot in shop['slots']:
         shop_item = shop_slot['obj']
         if shop_slot['slot_type'] == 'pet':
@@ -55,8 +75,9 @@ def conv_to_arr(player):
             # if any frozen pets exist, then the pet will be thought as frozen
             res[curr + 1 + 3*tiers.pet_tier_lookup_std[1].index(
                 shop_item['name'])] = 1 if shop_slot['frozen'] else 0
+            # cost of the pet (only 3 now since it doesn't change in the early rounds)
             res[curr + 2 + 3 * tiers.pet_tier_lookup_std[1].index(
-                shop_item['name'])] = 3
+                shop_item['name'])] = 3 
 
         elif shop_slot['slot_type'] == 'food':
             curr_index = tiers.food_tier_lookup[1].index(shop_item['name'])
